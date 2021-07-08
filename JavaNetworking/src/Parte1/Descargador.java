c9f2c763d9b3dce5d4b8b651d2199e3b6be166fd
Jorge Carrera Diez <55456507+jocarrd@users.noreply.github.com>
2021-07-08 18:47:22 +0200

    Bytes mal fragmentados

--- JavaNetworking/src/Parte1/Descargador.java
+++ JavaNetworking/src/Parte1/Descargador.java
@@ -9,6 +9,7 @@ import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.CountDownLatch;
 
 public class Descargador implements Runnable {
 
@@ -16,12 +17,15 @@ public class Descargador implements Runnable {
 	private File dir;
 	private long inicial;
 	private long fin;
+	private CountDownLatch countdown;
+	
 
-	public Descargador(String recurso, File dir, long inicial, long fin) {
+	public Descargador(String recurso, File dir, long inicial, long fin,CountDownLatch countdown) {
 		this.recurso = recurso;
 		this.dir = dir;
 		this.inicial = inicial;
 		this.fin = fin;
+		this.countdown=countdown;
 
 	}
 
@@ -35,12 +39,12 @@ public class Descargador implements Runnable {
 			con.addRequestProperty("Range", d);
 			
 			InputStream server = con.getInputStream();
-		//	DataOutputStream out = new DataOutputStream(new FileOutputStream(dir.getPath()+"/prueba"));
+		
 			
 			
 			
 			RandomAccessFile sr = new RandomAccessFile(this.dir+"/prueba.jpg", "rw");
-			sr.seek(this.inicial);
+			sr.seek(inicial);
 
 			int c = 0;
 			byte g[] = new byte[1000];
@@ -48,7 +52,7 @@ public class Descargador implements Runnable {
 			while ((c = server.read(g)) != -1) {
 				sr.write(g);
 			}
-
+			this.countdown.countDown();
 			server.close();
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
--- JavaNetworking/src/Parte1/Principal.java
+++ JavaNetworking/src/Parte1/Principal.java
@@ -6,6 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,6 +19,7 @@ public class Principal {
 		System.out.println("Introduzca la URL del destino a descargar");
 		String r = entrada.nextLine();
 		File dir;
+		CountDownLatch countdown = new CountDownLatch(3);
 		do {
 		System.out.println("Introduza el nombre del directorio donde desea guardarlo");
 		String path = entrada.nextLine();
@@ -32,27 +34,35 @@ public class Principal {
 			
 			for(int i=0;i<3;i++) {
 				
-				Descargador d;
-				if(i==2) {
-					 d = new Descargador(r,dir,i*(content_length/3),content_length);
-					System.out.println("Byte inicial :  "+(i*(content_length/3)));
-					System.out.println("Byte final :  "+((i+1)*(content_length/3)));
-				}else {
-					 d = new Descargador(r,dir,i*(content_length/3),i+1*(content_length/3));
-					System.out.println("Byte inicial :  "+(i*(content_length/3)));
-					System.out.println("Byte final :  "+((i+1)*(content_length/3)));
-					
+				Descargador d=null;
+				
+				if(i==0) {
+					 d = new Descargador(r,dir,0,content_length/3,countdown);
+					 System.out.println("Byte inicial :  "+0);
+						System.out.println("Byte final :  "+(content_length/3));
 				}
+				if(i==1 ) {
+					 d = new Descargador(r,dir,i*(content_length/3)+1,i+1*(content_length/3),countdown);
+					System.out.println("Byte inicial :  "+(i*(content_length/3)+1));
+					System.out.println("Byte final :  "+((i+1)*(content_length/3)));
+				}
+				
+				if(i==2) {
+					 d = new Descargador(r,dir,i*(content_length/3)+1,content_length,countdown);
+					System.out.println("Byte inicial :  "+(i*(content_length/3)+1));
+					System.out.println("Byte final :  "+content_length);
+				}
+				
 				
 				System.out.println("Total : "+content_length);
 				
 				pool.execute(d);
 			}
 			
+			System.out.println("Esperando a que terminen");
+			countdown.await();
 			
-
-			
-			
+			System.out.println("Descarga finalizada");
 			
 			pool.shutdown();
 		} catch (MalformedURLException e) {
@@ -60,6 +70,9 @@ public class Principal {
 			e.printStackTrace();
 		} catch (IOException e) {
 			
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
